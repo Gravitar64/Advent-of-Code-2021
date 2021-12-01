5e2aba462067b871b639af47aae94a2b547ccf1f
Gravitar64 <gravitar@web.de>
2021-12-01 11:51:33 +0100

    Tag 1

--- Tag_01.py
+++ Tag_01.py
@@ -1,14 +1,18 @@
 from time import perf_counter as pfc
 
+
 def puzzle_einlesen(datei):
   with open(datei) as f:
     return [int(x) for x in f]
 
+
 def löse(puzzle):
-  pass
-     
+  a = sum([b > a for a, b in zip(puzzle, puzzle[1:])])
+  b = sum([sum(puzzle[i+1:i+4]) > sum(puzzle[i:i+3])
+           for i in range(0, len(puzzle)-2)])
+  return a, b
 
 
 puzzle = puzzle_einlesen('Tag_01.txt')
 start = pfc()
-print(löse(puzzle),pfc()-start)
+print(löse(puzzle), pfc()-start)
