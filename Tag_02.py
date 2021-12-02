653d70537a572cf1a5bb4b7bea2dabf5e4a90d08
Gravitar64 <gravitar@web.de>
2021-12-02 10:42:55 +0100

    Create Tag_02.py

---
+++ Tag_02.py
@@ -0,0 +1,17 @@
+def read_puzzle(file):
+  with open(file) as f:
+    return [(row.split()[0], int(row.split()[1])) for row in f]
+
+
+def solve(puzzle):
+  horizontal = depth = aim = 0
+  for direction, x in puzzle:
+    if direction == 'forward': 
+      horizontal += x
+      depth      += aim * x
+    else:
+      aim += x if direction == 'down' else -x
+  return horizontal * aim, horizontal * depth  
+    
+    
+print(solve(read_puzzle('Tag_02.txt')))
