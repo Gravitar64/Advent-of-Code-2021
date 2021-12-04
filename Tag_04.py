52a88d08e1bdf92a6112fe5fc131138f73516afb
Gravitar64 <gravitar@web.de>
2021-12-04 13:48:37 +0100

    YouTube

--- Tag_04.py
+++ Tag_04.py
@@ -7,23 +7,22 @@ def read_puzzle(file):
 
 
 def checkBingo(board, draw_numbers, draw_number):
-  all_numbers = {n for zeile in board for n in zeile}
   row_cols = [{n for n in row} for row in board]
   row_cols += [{n for n in col} for col in zip(*board)]
   for s in row_cols:
-    if not s.issubset(draw_numbers):
-      continue
-    return sum(int(n) for n in (all_numbers - draw_numbers)) * int(draw_number)
+    if s.issubset(draw_numbers):
+      all_numbers = {n for row in board for n in row}
+      return sum(int(n) for n in (all_numbers - draw_numbers)) * int(draw_number)
 
 
 def solve(numbers, boards):
   draw_numbers, results = set(), []
   for draw_number in numbers:
     draw_numbers.add(draw_number)
-    for i, board in enumerate(boards):
+    for board in boards:
       if (bingo := checkBingo(board, draw_numbers, draw_number)):
         results.append(bingo)
-        del boards[i]
+        del boards[boards.index(board)]
   return results[0], results[-1]
 
 
