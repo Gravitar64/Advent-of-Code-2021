a5e43527224f337662fbc2bac35ebfc61636d751
Gravitar64 <gravitar@web.de>
2021-12-07 09:01:36 +0100

    part1 & 2 in einem Durchlauf

--- Tag_07.py
+++ Tag_07.py
@@ -1,22 +1,18 @@
-from time import perf_counter as pfc
-
-
 def read_puzzle(file):
   with open(file) as f:
     return sorted([int(x) for x in f.readline().split(',')])
 
 
-def solve(puzzle,part1):
+def solve(puzzle):
+  #part1
   length = len(puzzle)
   mid = puzzle[length//2]
-  if part1: return sum(abs(x-mid) for x in puzzle)
+  part1 = sum(abs(x-mid) for x in puzzle)
   
+  #part2
   mean = sum(puzzle) // length
   gauss = lambda x: x * (x+1) // 2
-  return sum(gauss(abs(x-mean)) for x in puzzle)  
+  return part1, sum(gauss(abs(x-mean)) for x in puzzle)  
 
 
-start = pfc()
-print(solve(read_puzzle('Tag_07.txt'), True))
-print(solve(read_puzzle('Tag_07.txt'), False))
-print(pfc()-start)
+print(solve(read_puzzle('Tag_07.txt')))
