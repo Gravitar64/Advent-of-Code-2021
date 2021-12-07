6e1a358c5623b8a631e0e8e69fbeaa6f7faceb3d
Gravitar64 <gravitar@web.de>
2021-12-07 08:49:36 +0100

    mathematisch optimiert (mid u. mean)

--- Tag_07.py
+++ Tag_07.py
@@ -1,23 +1,19 @@
 from time import perf_counter as pfc
 
+
 def read_puzzle(file):
   with open(file) as f:
-    return [int(x) for x in f.readline().split(',')]
-
-def gauss(x):
-  return x * (x+1) // 2
+    return sorted([int(x) for x in f.readline().split(',')])
 
 
 def solve(puzzle,part1):
-  low, high = min(puzzle), max(puzzle)
-  cheapest_possible_outcome = 99999999999999
-  for position in range(low,high+1):
-    if part1:
-      outcome = sum(abs(position-x) for x in puzzle)
-    else:
-      outcome = sum(gauss(abs(position-x)) for x in puzzle)
-    cheapest_possible_outcome = min(cheapest_possible_outcome, outcome)
-  return cheapest_possible_outcome  
+  length = len(puzzle)
+  mid = puzzle[length//2]
+  if part1: return sum(abs(x-mid) for x in puzzle)
+  
+  mean = sum(puzzle) // length
+  gauss = lambda x: x * (x+1) // 2
+  return sum(gauss(abs(x-mean)) for x in puzzle)  
 
 
 start = pfc()
