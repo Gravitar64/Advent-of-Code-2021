b7bcdc5d89b76917fff85f1523435cc583d5e6ad
Gravitar64 <gravitar@web.de>
2021-12-08 23:43:06 +0100

    lesbarkeit optimiert

--- Tag_08.py
+++ Tag_08.py
@@ -8,7 +8,7 @@ def read_puzzle(file):
     patterns, outputs = [], []
     for row in f:
       a,b = row.split('|')
-      patterns.append({len(x):set(x) for x in a.split()})
+      patterns.append({len(x):set(x) for x in a.split() if len(x) in {2,4}})
       outputs.append([set(x) for x in b.split()])
   return patterns, outputs    
 
@@ -17,17 +17,14 @@ def solve(patterns, outputs):
   part1 = sum(len(n) in {2, 4, 3, 7} for row in outputs for n in row)
   
   part2 = 0
-  t = {(6,3,2):'0', (2,2,2):'1', (5,2,1):'2', (5,3,2):'3', (4,4,2):'4',  
-       (5,3,1):'5', (6,3,1):'6', (3,2,2):'7', (7,4,2):'8', (6,4,2):'9'}
+  t = {(6,2,3):'0', (2,2,2):'1', (5,1,2):'2', (5,2,3):'3', (4,2,4):'4',  
+       (5,1,3):'5', (6,1,3):'6', (3,2,2):'7', (7,2,4):'8', (6,2,4):'9'}
   
   for pattern, output in zip(patterns,outputs):
-    part2 += int(''.join([
-                 t[(len(s),len(s&pattern[4]),len(s&pattern[2]))]          
-                 for s in output]))
-  
+    part2 += int(''.join(t[(len(s), len(s&pattern[2]), len(s&pattern[4]))] 
+                         for s in output))
   return part1, part2
 
 start = pfc()
 print(solve(*read_puzzle('Tag_08.txt')))
 print(pfc()-start)
-
