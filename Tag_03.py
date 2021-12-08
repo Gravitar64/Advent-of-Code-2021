cd6af6c9cc3dc0d57b3e4500015faa68851c2088
Gravitar64 <gravitar@web.de>
2021-12-08 12:09:35 +0100

    weitere Optimierungen

--- Tag_03.py
+++ Tag_03.py
@@ -20,18 +20,17 @@ def solve(puzzle):
   #Part 2
   filtered_oxy, filtered_co2 = puzzle.copy(), puzzle.copy()
   for i in range(len(puzzle[0])):
-    mC, _ = genCommon(list(zip(*filtered_oxy))[i])
-    _, lC = genCommon(list(zip(*filtered_co2))[i])
-
-    if len(filtered_oxy) > 1:
+    if filtered_oxy:
+      mC, _ = genCommon(list(zip(*filtered_oxy))[i])
       filtered_oxy = [row for row in filtered_oxy if row[i] == mC]
-    if len(filtered_oxy) == 1:
-      oxygen = filtered_oxy[0]
-
-    if len(filtered_co2) > 1:
+      if len(filtered_oxy) == 1:
+        oxygen = filtered_oxy.pop()
+    
+    if filtered_co2:
+      _, lC = genCommon(list(zip(*filtered_co2))[i])
       filtered_co2 = [row for row in filtered_co2 if row[i] == lC]
-    if len(filtered_co2) == 1:
-      co2 = filtered_co2[0]
+      if len(filtered_co2) == 1:
+        co2 = filtered_co2.pop()
 
   return int(gamma, 2) * int(epsilon, 2),  int(oxygen, 2) * int(co2, 2)
 
--- Tag_05.py
+++ Tag_05.py
@@ -8,19 +8,17 @@ def read_puzzle(file):
     return [[int(n) for n in re.findall('\d+', row)] for row in f]
 
 
-def solve(puzzle, part1):
-  diagram = Counter()
+def solve(puzzle):
+  diagram1,diagram2 = Counter(), Counter()
   for x1, y1, x2, y2 in puzzle:
-    if part1 and x1 != x2 and y1 != y2: continue 
     points = max(abs(x1-x2), abs(y1-y2))
     stepX, stepY = (x2-x1)//points, (y2-y1)//points
-    diagram.update([(x1 + n * stepX, y1 + n * stepY) for n in range(points+1)])
-  return sum(x > 1 for x in diagram.values())  
-  
+    if x1 == x2 or  y1 == y2:
+      diagram1.update([(x1 + n * stepX, y1 + n * stepY) for n in range(points+1)]) 
+    diagram2.update([(x1 + n * stepX, y1 + n * stepY) for n in range(points+1)]) 
+  return (sum(x > 1 for x in diagram1.values()),  
+          sum(x > 1 for x in diagram2.values()))
 
 start=pfc()
-print(solve(read_puzzle('Tag_05.txt'), True))
-print(pfc()-start)
-start=pfc()
-print(solve(read_puzzle('Tag_05.txt'),False))
+print(solve(read_puzzle('Tag_05.txt')))
 print(pfc()-start)
