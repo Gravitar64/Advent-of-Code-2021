652572a5bdd620d1e517f3c44a923219a3f9fa06
Gravitar64 <gravitar@web.de>
2021-12-08 15:59:45 +0100

    optimiert

--- Tag_08.py
+++ Tag_08.py
@@ -1,53 +1,33 @@
+#based on the idea of 4HbQ https://www.reddit.com/r/adventofcode/comments/rbj87a/2021_day_8_solutions/hnoyy04/?context=3
+
 from time import perf_counter as pfc
 
 
 def read_puzzle(file):
   with open(file) as f:
-    return [[x for x in row.split()] for row in f.readlines()]
+    patterns, outputs = [], []
+    for row in f:
+      a,b = row.split('|')
+      patterns.append({len(x):set(x) for x in a.split()})
+      outputs.append([set(x) for x in b.split()])
+  return patterns, outputs    
 
 
-def solve(puzzle):
-  part1 = sum(len(n) in {2, 4, 3, 7} for row in puzzle for n in row[11:])
+def solve(patterns, outputs):
+  part1 = sum(len(n) in {2, 4, 3, 7} for row in outputs for n in row)
   
   part2 = 0
-  segments = {*'abcdefg'}
-  unique_segments = {2: {*'cf'}, 4: {*'bcdf'}, 3: {*'acf'},
-                     7: {*'abcdefg'}, 5: {*'adg'}, 6: {*'abfg'}}
-  zahlen = {'abcefg': 0, 'cf': 1, 'acdeg': 2, 'acdfg': 3,
-            'bcdf': 4, 'abdfg': 5, 'abdefg': 6, 'acf': 7,
-            'abcdefg': 8, 'abcdfg': 9}
-
-  for row in puzzle:
-    translate = {}
-    wires = {a: {b for b in segments} for a in segments}
-    
-    for n in row[:10]:
-      for seg in unique_segments[len(n)]:
-        wires[seg] -= segments - {*n}
-    
-    changed = True
-    while changed:
-      changed = False
-      for key, values in wires.items():
-        if len(values) != 1: continue
-        translate[values.pop()] = key
-        changed = True
-      for key in translate:
-        for values in wires.values():
-          if key not in values: continue
-          values.remove(key)
-    
-    e = ''
-    for n in row[11:]:
-      decrypted = ''
-      for z in n:
-        decrypted += translate[z]
-      decrypted = ''.join(sorted(z for z in decrypted))
-      e += str(zahlen[decrypted])
-    part2 += int(e)
-
+  t = {632:'0', 222:'1', 521:'2', 532:'3', 442:'4',  
+       531:'5', 631:'6', 322:'7', 742:'8', 642:'9'}
+  
+  for pattern, output in zip(patterns,outputs):
+    part2 += int(''.join(
+                 [t[len(s)*100+len(s&pattern[4])*10+len(s&pattern[2])]          
+                 for s in output]))
+  
   return part1, part2
 
 start = pfc()
-print(solve(read_puzzle('Tag_08.txt')))
+print(solve(*read_puzzle('Tag_08.txt')))
 print(pfc()-start)
+
