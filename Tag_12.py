9e590d3b06c408e076408f2bba83c0e2717580f6
Gravitar64 <gravitar@web.de>
2021-12-12 19:15:23 +0100

    leichte Optimierungen

--- Tag_12.py
+++ Tag_12.py
@@ -5,25 +5,25 @@ from collections import defaultdict
 def read_puzzle(file):
   puzzle = defaultdict(list)
   with open(file) as f:
-    for row in f.readlines():
+    for row in f:
       a, b = row.strip().split('-')
       puzzle[a].append(b)
       puzzle[b].append(a)
   return puzzle
 
 
-def dfs(node, graph, seen, twice):
+def dfs(node, graph, visited, twice):
   paths = 0
   if node == 'end': return 1
   for neighb in graph[node]:
     if neighb.islower():
-      if neighb not in seen:
-        paths += dfs(neighb, graph, seen | {neighb}, twice)
+      if neighb not in visited:
+        paths += dfs(neighb, graph, visited | {neighb}, twice)
       elif twice and neighb not in {'start', 'end'}:
-        paths += dfs(neighb, graph, seen | {neighb}, False)
+        paths += dfs(neighb, graph, visited, False)
     else:
-      paths += dfs(neighb, graph, seen, twice)
-  return paths
+      paths += dfs(neighb, graph, visited, twice)
+  return paths    
 
 
 def solve(puzzle):
