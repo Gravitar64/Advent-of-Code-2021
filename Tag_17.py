703bc29d59457e53ba714c98f779d1ee6298e1aa
Gravitar64 <gravitar@web.de>
2021-12-17 22:28:23 +0100

    Optimiert part1

--- Tag_17.py
+++ Tag_17.py
@@ -1,27 +1,28 @@
 from time import perf_counter as pfc
 import re
 
+
 def read_puzzle(file):
   with open(file) as f:
-    return list(map(int, re.findall('-?\d+',f.read())))
-    
+    return list(map(int, re.findall('-?\d+', f.read())))
+
+
 def solve(minX, maxX, minY, maxY):
-  part1 = part2 = 0
-  for dy in range(minY,-minY):
-    for dx in range(1,maxX+1):
-      x = y = highest = 0
+  part1 = -minY * (-minY-1) // 2
+
+  part2 = 0
+  for dy in range(minY, -minY):
+    for dx in range(1, maxX+1):
+      x = y = 0
       vx, vy = dx, dy
       while x <= maxX and y >= minY:
-        x,y,highest = x+vx, y+vy, max(highest,y)
+        x, y = x+vx, y+vy
         vx, vy = max(0, vx-1), vy - 1
-        if minX <= x <= maxX and minY <= y <= maxY: 
-          part1 = max(highest,part1)
+        if minX <= x <= maxX and minY <= y <= maxY:
           part2 += 1
           break
-  return part1,part2
 
-
-
+  return part1, part2
 
 
 start = pfc()
