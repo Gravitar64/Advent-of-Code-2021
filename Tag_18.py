82f7e8aa1d91c87149af059af9a69545ba48708f
Gravitar64 <gravitar@web.de>
2021-12-19 19:55:05 +0100

    Lesbarkeit erhöht

--- Tag_18.py
+++ Tag_18.py
@@ -20,28 +20,31 @@ def gen_flatlist(puzzle):
   return flatlist
 
 
+def explode(x):
+  for i, ((num1, depth1), (num2, depth2)) in enumerate(zip(x, x[1:])):
+    if depth1 < 5 or depth1 != depth2: continue
+    if i > 0:          x[i-1][0] += num1
+    if i < len(x)-2:   x[i+2][0] += num2
+    return True, x[:i] + [[0, depth1-1]] + x[i+2:] 
+  return False, x
+
+
+def split(x):
+  for i, (num, depth) in enumerate(x):
+    if num < 10:  continue
+    down = num // 2
+    up = num - down
+    return True, x[:i] + [[down, depth+1],[up, depth+1]] + x[i+1:]
+  return False, x
+
+
 def add(a, b):
-  x = [[entry[0], entry[1]+1] for entry in a + b]
-  change = True
-  while change:
-    change = False
-    #explode
-    for i, ((num1, depth1), (num2, depth2)) in enumerate(zip(x, x[1:])):
-      if depth1 < 5 or depth1 != depth2: continue
-      if i > 0:          x[i-1][0] += num1
-      if i < len(x)-2:   x[i+2][0] += num2
-      x = x[:i] + [[0, depth1-1]] + x[i+2:]
-      change = True
-      break
-    #split
-    if not change:
-      for i, (num, depth) in enumerate(x):
-        if num < 10:  continue
-        down = num // 2
-        up = num - down
-        x = x[:i] + [[down, depth+1]] + [[up, depth+1]] + x[i+1:]
-        change = True
-        break
+  x = [[num, depth+1] for num,depth in a + b]
+  while True:
+    change, x = explode(x)
+    if change: continue
+    change,x = split(x)
+    if not change: break
   return x
 
 
