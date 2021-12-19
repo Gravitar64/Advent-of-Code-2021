a505db1190d0328bcb7a8fcda57f9a1563b80837
Gravitar64 <gravitar@web.de>
2021-12-19 18:03:23 +0100

    Lösung mithilfe einer flachen liste (wert, tiefe)

--- Tag_18.py
+++ Tag_18.py
@@ -1,71 +1,69 @@
-# Sourcecode by Benedikt Werner https://github.com/benediktwerner/AdventOfCode/blob/master/2021/day18/sol.py
-# Dient als Anschauungsbeispiel für eine rekursive Lösung in 3,72 Sekunden 
-
 from time import perf_counter as pfc
-import itertools, math
 from functools import reduce
+from itertools import permutations
 
 
-def read_puzzle(file):
-  with open(file) as f:
-    return list(map(eval, f.read().splitlines()))
+def read_puzzle(filename):
+  with open(filename) as f:
+    return f.readlines()
 
 
-def add_left(x, n):
-  if n is None: return x
-  if isinstance(x, int): return x + n
-  return [add_left(x[0], n), x[1]]
+def gen_flatlist(puzzle):
+  flatlist = []
+  for line in puzzle:
+    flatline, depth = [], 0
+    for c in line:
+      if c == '[':       depth += 1
+      elif c == ']':     depth -= 1
+      elif c.isdigit():  flatline.append([int(c), depth])
+    flatlist.append(flatline)
+  return flatlist
 
 
-def add_right(x, n):
-  if n is None:  return x
-  if isinstance(x, int):  return x + n
-  return [x[0], add_right(x[1], n)]
-
-
-def explode(x, n=4):
-  if isinstance(x, int): return False, None, x, None
-  if n == 0: return True, x[0], 0, x[1]
-  
-  a, b = x
-  exp, left, a, right = explode(a, n - 1)
-  if exp:  return True, left, [a, add_left(b, right)], None
-  
-  exp, left, b, right = explode(b, n - 1)
-  if exp:  return True, None, [add_right(a, left), b], right
-  
-  return False, None, x, None
+def explode(x):
+  for i, ((num1, depth1), (num2, depth2)) in enumerate(zip(x, x[1:])):
+    if depth1 < 5 or depth1 != depth2: continue
+    if i > 0:          x[i-1][0] += num1
+    if i < len(x)-2:   x[i+2][0] += num2
+    x = x[:i] + [[0, depth1-1]] + x[i+2:]
+    return True, x
+  return False, x
 
 
 def split(x):
-  if isinstance(x, int):
-    if x >= 10: return True, [x // 2, math.ceil(x / 2)]
-    return False, x
-  a, b = x
-  change, a = split(a)
-  if change: return True, [a, b]
-  change, b = split(b)
-  return change, [a, b]
+  for i, (num, depth) in enumerate(x):
+    if num < 10:  continue
+    down = num // 2
+    up = num - down
+    x = x[:i] + [[down, depth+1]] + [[up, depth+1]] + x[i+1:]
+    return True, x
+  return False, x
 
 
 def add(a, b):
-  x = [a, b]
+  x = [[entry[0], entry[1]+1] for entry in a + b]
   while True:
-    change, _, x, _ = explode(x)
-    if change: continue
+    change, x = explode(x)
+    if change:  continue
     change, x = split(x)
     if not change: break
   return x
 
 
 def magnitude(x):
-  if isinstance(x, int): return x
-  return 3 * magnitude(x[0]) + 2 * magnitude(x[1])
+  while len(x) > 1:
+    for i, ((num1, depth1), (num2, depth2)) in enumerate(zip(x, x[1:])):
+      if depth1 != depth2: continue
+      val = num1 * 3 + num2 * 2
+      x = x[:i]+[[val, depth1-1]]+x[i+2:]
+      break
+  return x[0][0]
 
 
 def solve(puzzle):
-  part1 = magnitude(reduce(add, puzzle))
-  part2 = max(magnitude(add(a, b)) for a, b in itertools.permutations(puzzle, 2))
+  flatlist = gen_flatlist(puzzle)
+  part1 = magnitude(reduce(add, flatlist))
+  part2 = max(magnitude(add(a, b)) for a, b in permutations(flatlist, 2))
   return part1, part2
 
 
