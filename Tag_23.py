12d63c0b633a667b4473963e5c9537a3e2ee09a6
Gravitar64 <gravitar@web.de>
2021-12-26 11:03:34 +0100

    V0.2

--- Tag_23.py
+++ Tag_23.py
@@ -1,84 +1,70 @@
 from time import perf_counter as pfc
-from collections import defaultdict
 
 #Ideen
 #1. kann ein amphipod aus der hallway in seinen Raum wechseln
 #2. kann ein amphipod seinen raum in die hallway verlassen
 
 def read_puzzle(filename):
-  walls, amphipods,rooms = set(), defaultdict(list), defaultdict(list)
   with open(filename) as f:
-    for y,row in enumerate(f.read().split('\n')):
-      for x,c in enumerate(row):
-        if c == '#': walls.add((x,y))
-        if c in 'ABCD': 
-          amphipods[c].append((x,y))
-          rooms[x].append((c,y))
+    return {(x,y):c for y,row in enumerate(f.read().split('\n')) for x,c in enumerate(row) if c not in'# '}
 
-  return walls, amphipods, rooms
+def room_solved(room, puzzle,targetsI):
+  return all(puzzle[(room,y)] == targetsI[room] for y in range(1,3))
 
-def get_free_hallway_pos(hallway):
-  return {pos for pos,state in hallway.items() if state == ''}
+def can_leave_room(room, puzzle, targetsI):
+  if room_solved(room, puzzle, targetsI): return False
+  if puzzle[(room,2)] in 'ABCD': return (room,2)
+  if puzzle[(room,3)] in 'ABCD' and puzzle[(room,3)] != targetsI[room]: return (room,3)
 
-
-def reachable(x1,y1,x2,y2,amphipods):
-  if any((x1,ny) in amphipods.values() for ny in range(y1,y2+1)): return False
-  if any((nx,y2) in amphipods.values() for nx in range(x1,x2+1)): return False
-  return True
-
-def target_reached(c,x,y):
-  if y != targets[c]: return False
-    
-def room_solved(room,renter,targets):
-  return all(room == targets[amphi] for amphi,_ in renter)
+def can_enter_room(x,puzzle,targets):
+  amphi = puzzle[(x,1)]
+  room = targets[amphi]
+  if all(puzzle[(room,y)] == '.' for y in range(2,4)): return room,3
+  if puzzle[(room,3)] == amphi and puzzle[(room,2)] == '.': return room,2
 
 
-def leave_room(c,x1,y1):
-  if not (points := get_free_hallway_pos()): return False
-  return {(x2,y2) for x,y in hallway if reachable(x1,y1,x2,y2)}
+def blocked(x1,x2,puzzle):
+  step = 1 if x1 < x2 else -1
+  return any(puzzle[v,1] != '.' for v in range(x1+step,x2+step,step))
 
-
-def get_board_pos(amphipods):
-  board_pos = ''
-  for amphi,x,y in sorted(amphipods):
-    board_pos += f'{amphi} {x} {y}'
-  return board_pos
+def get_possible_hallway_pos(x,_,hallway,puzzle):
+  for x2 in hallway:
+    if puzzle[(x2,1)] != '.': continue
+    if blocked(x,x2,puzzle): continue
+    yield (x2,1)  
 
 def distance(x1,y1,x2,y2):
-  return abs(x2-x1)+abs(y2-y1)      
+  return abs(x2-x1)+abs(y2-y1)
 
-def solve(walls, amphipods, rooms):
+def get_mapStr(puzzle):
+  return ''.join(puzzle.values())        
+
+def solve(puzzle):
   energy = dict(A=1, B=10, C=100, D=1000)
-  hallway = {(1,1):'',(2,1):'',(4,1):'',(6,1):'',(8,1):'',(10,1):'',(11,1):''}
-  targets = {'A':3, 'B':5, 'C':'7', 'D':9, '.' :None}
-  cost = 0
-  # enter target-room from hallway?
-  for pos,amphi in hallway:
-    if amphi == '': continue
-  
-  # leave room to hallway?
-  for x, renter in rooms.items():
-    if room_solved(x,renter,targets): continue
-    for ri, (amphi, y) in enumerate(renter):
-      if amphi == '.': continue
-      if targets[amphi] == x and y==3: continue
-      if y in {2,3}:
-        for pos2 in get_free_hallway_pos(hallway):
-          if not reachable(x,y,*pos2,amphipods): continue
-          cost += distance(x,y,*pos2) * energy[amphi]
-          rooms[x][ri] = ('.',y)
-          hallway[pos2] = amphi
-          amphi_poss = amphipods[amphi]
-          ai = amphi_poss.index((x,y))
-          amphipods[amphi][ai] = pos2
-          print(rooms)
-          print(hallway)
-          print(amphipods)
+  hallway = {1,2,4,6,8,10,11}
+  targets = {'A':3, 'B':5, 'C':7, 'D':9}
+  targetsI = {val:key for key,val in targets.items()}
+  print(get_mapStr(puzzle))
+  #1. Hallway to room possible?
+  for x in hallway:
+    amphi = puzzle[(x,1)]
+    if amphi == '.': continue
+    if (pos2 := can_enter_room(x,puzzle,targets)):
+      if not blocked(x,pos2[0],puzzle):
+        print((x,1),pos2,distance(x,1, *pos2),' YES')
+      
 
-   
-
-
+  #2. Leave room possible?
+  for room in targetsI:
+    if (pos1 :=can_leave_room(room, puzzle, targetsI)):
+      for pos2 in get_possible_hallway_pos(*pos1, hallway, puzzle):
+        print(pos1,pos2,distance(*pos1, *pos2))
+        # amphi = puzzle[pos1]
+        # puzzle[pos1] = '.'
+        # hallway[pos2] = 'amphi'
+      
+    
 
 start = pfc()
-print(solve(*read_puzzle('Tag_23.txt')))
+print(solve(read_puzzle('Tag_23.txt')))
 print(pfc()-start)
