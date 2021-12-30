8f5291d8d59c576927a187818a1db0cbea2cb994
Gravitar64 <gravitar@web.de>
2021-12-30 13:45:10 +0100

    V0.8 optimiert mit Dijkstra 2.5 Sek.

--- Tag_23.py
+++ Tag_23.py
@@ -1,118 +1,107 @@
 from time import perf_counter as pfc
 from heapq import heappush, heappop
 
+
 def read_puzzle(filename):
     with open(filename) as f:
-        return {(x, y): c for y, row in enumerate(f.read().split('\n')) for x, c in enumerate(row) if c not in '# '}
+        return "".join(
+            [c for row in f.read().split("\n") for c in row if c not in "# "]
+        )
 
 
-def room_solved(room, puzzle, targetsI):
-    return all(puzzle[(room, y)] == targetsI[room] for y in range(1, 3))
+def room_solved(room, puzzle, targets):
+    return all(puzzle[i] == room for i in targets[room])
 
 
-def can_leave_room(room, puzzle, targetsI):
-    if room_solved(room, puzzle, targetsI): return False
-    if puzzle[(room, 2)] in 'ABCD': return (room, 2)
-    if puzzle[(room, 3)] in 'ABCD' and \
-       puzzle[(room, 3)] != targetsI[room]: return (room, 3)
+def can_leave_room(room, puzzle, targets):
+    if room_solved(room, puzzle, targets):
+        return False
+    for n, i in enumerate(targets[room]):
+        if puzzle[i] != ".":
+            if n == 0:
+                return i
+            elif puzzle[i] != room:
+                return i
 
 
 def can_enter_room(amphi, puzzle, targets):
-    room = targets[amphi]
-    if all(puzzle[(room, y)] == '.' for y in range(2, 4)): return room, 3
-    if puzzle[(room, 3)] == amphi and \
-       puzzle[(room, 2)] == '.': return room, 2
+    if room_solved(amphi, puzzle, targets):
+        return False
+    for i in targets[amphi]:
+        if puzzle[i] == ".":
+            bestI = i
+        elif puzzle[i] != amphi:
+            return False
+    return bestI
 
 
-def blocked(x1, x2, puzzle):
-    step = 1 if x1 < x2 else -1
-    return any(puzzle[v, 1] != '.' for v in range(x1+step, x2+step, step))
+def blocked(i1, i2, puzzle):
+    step = 1 if i1 < i2 else -1
+    return any(puzzle[v] != "." for v in range(i1 + step, i2 + step, step))
 
 
-def get_possible_hallway_pos(x, _, hallway, puzzle):
-    for x2 in hallway:
-        if puzzle[(x2, 1)] != '.': continue
-        if blocked(x, x2, puzzle): continue
-        yield (x2, 1)
+def get_possible_hallway_pos(i1, hallway, puzzle, stepout, targetsI):
+    for i2 in hallway:
+        if puzzle[i2] != ".":
+            continue
+        if blocked(stepout[targetsI[i1]], i2, puzzle):
+            continue
+        yield i2
 
 
-def distance(x1, y1, x2, y2):
-    return abs(x2-x1)+abs(y2-y1)
+def distance(i1, i2, stepout, targetsI):
+    f, t = min(i1, i2), max(i1, i2)
+    step = stepout[targetsI[t]]
+    d = abs(step - f) + (t-7)//4
+    return d
 
 
-def get_mapStr(puzzle):
-    return ''.join(puzzle.values())
-
-
-def swap(p1, p2, puzzle):
-    puzzle[p2], puzzle[p1] = puzzle[p1], puzzle[p2]
-    return puzzle
+def swap(i1, i2, puzzle):
+    p = list(puzzle)
+    p[i1], p[i2] = p[i2], p[i1]
+    return "".join(p)
 
 
 def possible_moves(puzzle):
-    moves =[]
-    for x in hallway:
-        p1 = (x,1)
-        amphi = puzzle[p1]
-        if amphi == '.': continue
-        if not (p2 := can_enter_room(amphi, puzzle, targets)): continue    
-        if blocked(p1[0], p2[0], puzzle): continue
-        moves.append((p1,p2,distance(*p1,*p2),amphi))
-    for room in targetsI:
-        if not (p1 := can_leave_room(room, puzzle, targetsI)): continue
-        amphi = puzzle[p1]
-        for p2 in get_possible_hallway_pos(*p1, hallway, puzzle):
-            moves.append((p1,p2,distance(*p1,*p2),amphi))
+    moves = []
+    for i1 in hallway:
+        if puzzle[i1] == ".":
+            continue
+        if not (i2 := can_enter_room(puzzle[i1], puzzle, targets)):
+            continue
+        if blocked(i1, stepout[puzzle[i1]], puzzle):
+            continue
+        moves.append((i1, i2, distance(i1, i2, stepout, targetsI)))
+    for room in "ABCD":
+        if not (i1 := can_leave_room(room, puzzle, targets)):
+            continue
+        for i2 in get_possible_hallway_pos(i1, hallway, puzzle, stepout, targetsI):
+            moves.append((i1, i2, distance(i1, i2, stepout, targetsI)))
     return moves
-
-#Problem beim dfs ist, dass bereits erreichte Konstellationen über <if state in seen> 
-#übersprungen werden, obwohl diese Konstellation über eine günstigere Zugfolge erreicht
-#wurde. Nur über Dijkstra oder A* ist die lösbar, da dort nur dann in seen übersprungen wird,
-#falls die Kosten höher sind, als die dort beim State gespeicherten Kosten
-#
-#Deshalb wurde jetzt hier seen als Dictionary implementiert, mit dem State als Key und den 
-#bis dahin optimal gefundenen Kosten. Ein Überspringen findet nur dann statt, wenn die 
-#bisher gefundenen Kosten zu diesem State geringer sind, als die aktuellen Kosten
-
-results = []
-def dfs(cost,puzzle,path):
-    global minCost
-    state = get_mapStr(puzzle)
-    if state == '...........ABCDABCD':
-        return cost, path
-    for p1,p2,dist,amphi in possible_moves(puzzle):
-        moved = swap(p1,p2,puzzle.copy())
-        state = get_mapStr(moved)
-        new_cost = cost + dist * energy[amphi]
-        if seen.get(state, 999999) <= new_cost: continue
-        seen[state] = new_cost
-        erg, p = dfs(new_cost, moved, path+[state])
-        if erg and erg < minCost:
-            minCost = erg
-            print(erg)
-            results.append((minCost,p))
-    return False, None
-
 
 
 def solve(puzzle):
-    dfs(0, puzzle, [get_mapStr(puzzle)])
-    cost, path = sorted(results)[0]
-    for move in path:
-        print(move[:11])
-        print(2*'#'+move[11]+'#'+move[12]+'#'+move[13]+'#'+move[14]+'#')
-        print(2*'#'+move[15]+'#'+move[16]+'#'+move[17]+'#'+move[18]+'#')
-        print()
-    print(cost)
+    queue, seen = [(0, puzzle)], {puzzle: 0}
+    while queue:
+        cost, state = heappop(queue)
+        if state == "...........ABCDABCD":
+            return cost
+        for i1, i2, dist in possible_moves(state):
+            new_cost = cost + dist * energy[state[i1]]
+            moved = swap(i1, i2, state)
+            if seen.get(moved, 999999) <= new_cost:
+                continue
+            seen[moved] = new_cost
+            heappush(queue, (new_cost, moved))
+
+
+energy = dict(A=1, B=10, C=100, D=1000)
+hallway = {0, 1, 3, 5, 7, 9, 10}
+stepout = {"A": 2, "B": 4, "C": 6, "D": 8}
+targets = {"A": [11, 15], "B": [12, 16], "C": [13, 17], "D": [14, 18]}
+targetsI = {v: key for key, val in targets.items() for v in val}
+
 
 start = pfc()
-puzzle = read_puzzle('Tag_23.txt')
-save_state = get_mapStr(puzzle)
-energy = dict(A=1, B=10, C=100, D=1000)
-hallway = {1, 2, 4, 6, 8, 10, 11}
-targets = {'A': 3, 'B': 5, 'C': 7, 'D': 9}
-targetsI = {val: key for key, val in targets.items()}
-minCost, seen = 1<<32, {get_mapStr(puzzle):0}
-
-print(solve(puzzle))
-print(pfc()-start)
+print(solve(read_puzzle("Tag_23.txt")))
+print(pfc() - start)
