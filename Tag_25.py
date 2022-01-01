5fbcf259405901876e352ea17957fa02df7e0a44
Gravitar64 <gravitar@web.de>
2022-01-01 19:02:58 +0100

    V0.2 1.4 Sek.

--- Tag_25.py
+++ Tag_25.py
@@ -2,47 +2,51 @@ from time import perf_counter as pfc
 
 
 def read_puzzle(filename):
+    vs, hs = set(), set()
     with open(filename) as f:
-        return {(x,y):c for y,row in enumerate(f.read().split("\n"))
-                        for x,c in enumerate(row) if c != '.'}
+        for y, row in enumerate(f.read().split("\n")):
+            for x, c in enumerate(row):
+                if c == "v":
+                    vs.add((x, y))
+                elif c == ">":
+                    hs.add((x, y))
+    return hs, vs
 
 
-def nextPos(x,y,c,w,h):
-    if c == '>': return (x+1)%w, y
-    else:        return x, (y+1)%h
+def nextPos(x, y, c, w, h):
+    if c == ">":
+        return (x + 1) % w, y
+    else:
+        return x, (y + 1) % h
 
 
-def solve(puzzle):
-    w,h = max([x for x,y in puzzle])+1, max([y for x,y in puzzle])+1
-    oldState = set(puzzle)
-    step = 0
+def findStable(hs, vs, w, h, step = 0):
     while True:
         step += 1
-        nextGen = {}
-        for pos,c in puzzle.items():
-            if c != '>': 
-                nextGen[pos] = c
-                continue
-            newPos = nextPos(*pos,c,w,h)
-            if newPos in puzzle:
-                nextGen[pos] = c
+        new_hs = set()
+        for pos in hs:
+            newPos = nextPos(*pos, ">", w, h)
+            if newPos in hs or newPos in vs:
+                new_hs.add(pos)
             else:
-                nextGen[newPos] = c
-        puzzle = nextGen
-        nextGen = {}
-        for pos,c in puzzle.items():
-            if c != 'v': 
-                nextGen[pos] = c
-                continue
-            newPos = nextPos(*pos,c,w,h)
-            if newPos in puzzle:
-                nextGen[pos] = c
+                new_hs.add(newPos)
+        new_vs = set()
+        for pos in vs:
+            newPos = nextPos(*pos, "v", w, h)
+            if newPos in vs or newPos in new_hs:
+                new_vs.add(pos)
             else:
-                nextGen[newPos] = c             
-        puzzle = nextGen
-        if set(puzzle) == oldState: return step
-        oldState = set(puzzle)
+                new_vs.add(newPos)
+        if vs == new_vs and hs == new_hs: return step
+        hs, vs = new_hs, new_vs
+
+
+def solve(hs, vs):
+    all_pos = hs | vs
+    w, h = map(max, zip(*all_pos))
+    return findStable(hs, vs, w + 1, h + 1)
+
 
 start = pfc()
-print(solve(read_puzzle("Tag_25.txt")))
+print(solve(*read_puzzle("Tag_25.txt")))
 print(pfc() - start)
