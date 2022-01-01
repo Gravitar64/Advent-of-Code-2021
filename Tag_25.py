456a36cbdb6d00b0e70e2452c6b1d0092c7ed718
Gravitar64 <gravitar@web.de>
2022-01-01 20:29:38 +0100

    V1.0 numpy magic in 0.3 Sek.

--- Tag_25.py
+++ Tag_25.py
@@ -1,41 +1,26 @@
+# Geniale Lösung von GrAndAG77 mit numpy
+# https://www.reddit.com/r/adventofcode/comments/ro2uav/2021_day_25_solutions/hpwntr6/?context=3
+
 from time import perf_counter as pfc
+import numpy as np
 
 
 def read_puzzle(filename):
-    vs, hs = set(), set()
     with open(filename) as f:
-        for y, row in enumerate(f.read().split("\n")):
-            for x, c in enumerate(row):
-                if c == "v":    vs.add((x, y))
-                elif c == ">":  hs.add((x, y))
-    return hs, vs
+        return np.array(list(map(list, f.read().strip().split())))
 
 
-def nextPos(x, y, c, w, h):
-    if c == ">": return (x + 1) % w, y
-    else:        return x, (y + 1) % h
-
-
-def findStable(hs, vs, w, h, step = 0):
-    while True:
-        step += 1
-        new_hs, new_vs = set(), set() 
-        for pos in hs:
-            newPos = nextPos(*pos, ">", w, h)
-            new_hs.add(pos) if newPos in hs or newPos in vs else new_hs.add(newPos)
-        for pos in vs:
-            newPos = nextPos(*pos, "v", w, h)
-            new_vs.add(pos) if newPos in new_hs or newPos in vs else new_vs.add(newPos)
-        if hs == new_hs and vs == new_vs: return step
-        hs, vs = new_hs, new_vs
-
-
-def solve(hs, vs):
-    all_pos = hs | vs
-    w, h = map(max, zip(*all_pos))
-    return findStable(hs, vs, w + 1, h + 1)
+def solve(puzzle):
+    moved = [True]
+    while any(moved[-2:]):
+        for c, axis in ((">", 1), ("v", 0)):
+            allowed_moves = (np.roll(puzzle, -1, axis) == ".") & (puzzle == c)
+            moved.append(np.any(allowed_moves))
+            puzzle[allowed_moves] = "."
+            puzzle[np.roll(allowed_moves, 1, axis)] = c
+    return len(moved) // 2
 
 
 start = pfc()
-print(solve(*read_puzzle("Tag_25.txt")))
+print(solve(read_puzzle("Tag_25.txt")))
 print(pfc() - start)
