4810e3c014c32e0b4dd063c97b7150d9eebf8532
Gravitar64 <gravitar@web.de>
2022-01-15 16:01:45 +0100

    YouTube2

--- Tag_23.py
+++ Tag_23.py
@@ -1,80 +1,77 @@
 from time import perf_counter as pfc
-from itertools import product
 from heapq import heappop, heappush
 
 
 def read_puzzle(filename):
   with open(filename) as f:
-    return ''.join(c for c in f.read() if c not in '#  \n')
+    return ''.join([c for c in f.read() if c in 'ABCD.'])
 
 
-def can_enter_room(a, b, amphi, puzzle, t):
-  bestI = False
-  for i in t:
-    if puzzle[i] == '.': bestI = i
-    elif puzzle[i] != amphi: return False
-  if not blocked(a, b, puzzle): return bestI
-
-
-def blocked(a, b, puzzle):
-  step = 1 if a < b else -1
-  for i in range(a+step, b+step, step):
-    if puzzle[i] != '.': return True
-
-
-def can_leave_room(room, puzzle, t):
-  if all(puzzle[i] == room for i in t if puzzle[i] != '.'): return False
-  for a in t:
+def can_leave_room(puzzle, room_pos):
+  for a in room_pos:
     if puzzle[a] == '.': continue
     return a
 
 
-def get_possible_hallway_pos(a, hallway, puzzle):
-  for b in [i for i in hallway if puzzle[i] == '.']:
+def blocked(a,b,puzzle):
+  step = 1 if a<b else -1
+  for pos in range(a+step, b+step, step):
+    if puzzle[pos] != '.': return True    
+
+
+def get_possible_parc_pos(a,parc,puzzle):
+  for b in [pos for pos in parc if puzzle[pos] == '.']:
     if blocked(a,b,puzzle): continue
     yield b
 
 
-def move(a, b, puzzle):
+def move(a,b,puzzle):
   p = list(puzzle)
   p[a], p[b] = p[b], p[a]
-  return "".join(p)
+  return ''.join(p)
 
 
-def possible_moves(puzzle, hallway,stepout,target):
-  for a in hallway:
+def can_enter_room(a,b,amphi,puzzle,room_pos):
+  for pos in room_pos:
+    if puzzle[pos] == '.': best_pos = pos
+    elif puzzle[pos] != amphi: return False
+  if not blocked(a,b,puzzle): return best_pos
+
+
+def possible_moves(puzzle, parc, stepout, target):
+  for a in [pos for pos in parc if puzzle[pos] != '.']:
     amphi = puzzle[a]
-    if amphi == '.': continue
-    if not (b := can_enter_room(a, stepout[amphi], amphi, puzzle, target[amphi])): continue
-    yield a,b
-  for room in "ABCD":
-    if not (a := can_leave_room(room, puzzle, target[room])): continue
-    for b in get_possible_hallway_pos(stepout[room],hallway,puzzle):
-      yield a,b        
+    if (b:=can_enter_room(a, stepout[amphi], amphi, puzzle, target[amphi])):
+      yield a,b
+  for room in 'ABCD':
+    if not (a:=can_leave_room(puzzle, target[room])): continue
+    for b in get_possible_parc_pos(stepout[room], parc, puzzle):
+      yield a,b
 
 
 def solve(puzzle):
   energy = dict(A=1, B=10, C=100, D=1000)
-  hallway = {0, 1, 3, 5, 7, 9, 10}
+  parc= [0,1,3,5,7,9,10]
   stepout = dict(A=2, B=4, C=6, D=8)
-  target = {room: list(range(11+i, len(puzzle), 4)) for i, room in enumerate(stepout)}
-  targetI = {v: key for key, val in target.items() for v in val}
-  
-  queue, seen = [(0, puzzle)], {puzzle: 0}
-  solution = '.'*11+'ABCD'*2 if len(puzzle) == 19 else '.'*11+'ABCD'*4
-  while queue:
-    cost, state = heappop(queue)
+  target = {r: range(ord(r)-54,len(puzzle),4) for r in 'ABCD'}
+  targetI = {v:key for key,val in target.items() for v in val}
+
+  solution = '.'*11+'ABCD'*((len(puzzle)-11)//4)
+  heap, seen = [(0,puzzle)], {puzzle:0}
+  while heap:
+    cost, state = heappop(heap)
     if state == solution: return cost
-    for a, b in possible_moves(state, hallway, stepout, target):
-      a1,b1 = (a,b) if a < b else (b,a)
-      distance = abs(stepout[targetI[b1]] - a1) + (b1-7)//4
+    for a,b in possible_moves(state, parc, stepout, target):
+      p,r = (a,b) if a < b else (b,a)
+      distance = abs(stepout[targetI[r]] - p) + (r-7)//4
       new_cost = cost + distance * energy[state[a]]
-      moved = move(a, b, state)
-      if seen.get(moved, 999999) <= new_cost: continue
+      moved = move(a,b,state)
+      if seen.get(moved,999999) <= new_cost: continue
       seen[moved] = new_cost
-      heappush(queue, (new_cost, moved))
+      heappush(heap,(new_cost, moved))
+  
 
-
+  
 start = pfc()
 print(solve(read_puzzle("Tag_23_a.txt")))
 print(solve(read_puzzle("Tag_23_b.txt")))
