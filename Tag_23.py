fc2019b9f51c80848deeb8b6251e11395b4b41f2
Gravitar64 <gravitar@web.de>
2022-01-15 10:40:17 +0100

    Youtube

--- Tag_23.py
+++ Tag_23.py
@@ -1,110 +1,81 @@
 from time import perf_counter as pfc
-from heapq import heappush, heappop
 from itertools import product
+from heapq import heappop, heappush
 
 
 def read_puzzle(filename):
   with open(filename) as f:
-    return ''.join([c for row in f.read().split("\n") for c in row if c not in "# "])
+    return ''.join(c for c in f.read() if c not in '#  \n')
 
 
-def can_leave_room(room, puzzle, part1):
-  t = targets1[room] if part1 else targets2[room]
-  if all(puzzle[i] == room for i in t if puzzle[i] != '.'):
-    return False
-  for i in t:
-    if puzzle[i] != '.':
-      return i
-
-
-def can_enter_room(i1, amphi, puzzle, part1):
-  t = targets1[amphi] if part1 else targets2[amphi]
+def can_enter_room(a, b, amphi, puzzle, t):
   bestI = False
   for i in t:
-    if puzzle[i] == ".":
-      bestI = i
-    elif puzzle[i] != amphi:
-      return False
-  if not blocked(i1, stepout[amphi], puzzle):
-    return bestI
+    if puzzle[i] == '.': bestI = i
+    elif puzzle[i] != amphi: return False
+  if not blocked(a, b, puzzle): return bestI
 
 
-def blocked(i1, i2, puzzle):
-  step = 1 if i1 < i2 else -1
-  for v in range(i1+step, i2+step, step):
-    if puzzle[v] != '.':
-      return True
+def blocked(a, b, puzzle):
+  step = 1 if a < b else -1
+  for i in range(a+step, b+step, step):
+    if puzzle[i] != '.': return True
 
 
-def get_possible_hallway_pos(i1, puzzle):
-  out = stepout[targetsI[i1]]
-  for i2 in left[out]:
-    if puzzle[i2] != ".":
-      break
-    yield i2
-  for i2 in right[out]:
-    if puzzle[i2] != ".":
-      break
-    yield i2
+def can_leave_room(room, puzzle, t):
+  if all(puzzle[i] == room for i in t if puzzle[i] != '.'): return False
+  for a in t:
+    if puzzle[a] == '.': continue
+    return a
 
 
-def gen_distances():
-  distances = {}
-  for a, b in product(hallway, range(11, 27)):
-    distances[(a, b)] = abs(stepout[targetsI[b]] - a) + (b-7)//4
-    distances[(b, a)] = distances[(a, b)]
-  return distances
+def get_possible_hallway_pos(a, hallway, puzzle):
+  for b in [i for i in hallway if puzzle[i] == '.']:
+    if blocked(a,b,puzzle): continue
+    yield b
 
 
-def swap(i1, i2, puzzle):
+def move(a, b, puzzle):
   p = list(puzzle)
-  p[i1], p[i2] = p[i2], p[i1]
+  p[a], p[b] = p[b], p[a]
   return "".join(p)
 
 
-def possible_moves(puzzle, part1):
-  for i1 in hallway:
-    if puzzle[i1] == ".":
-      continue
-    if not (i2 := can_enter_room(i1, puzzle[i1], puzzle, part1)):
-      continue
-    yield i1, i2
+def possible_moves(puzzle, hallway,stepout,target):
+  for a in hallway:
+    amphi = puzzle[a]
+    if amphi == '.': continue
+    if not (b := can_enter_room(a, stepout[amphi], amphi, puzzle, target[amphi])): continue
+    yield a,b
   for room in "ABCD":
-    if not (i1 := can_leave_room(room, puzzle, part1)):
-      continue
-    for i2 in get_possible_hallway_pos(i1, puzzle):
-      yield i1, i2
+    if not (a := can_leave_room(room, puzzle, target[room])): continue
+    for b in get_possible_hallway_pos(stepout[room],hallway,puzzle):
+      yield a,b        
 
 
-def solve(puzzle, part1=True):
+def solve(puzzle):
+  energy = dict(A=1, B=10, C=100, D=1000)
+  hallway = {0, 1, 3, 5, 7, 9, 10}
+  stepout = dict(A=2, B=4, C=6, D=8)
+  target = {room: list(range(11+i, len(puzzle), 4)) for i, room in enumerate(stepout)}
+  targetI = {v: key for key, val in target.items() for v in val}
+  
   queue, seen = [(0, puzzle)], {puzzle: 0}
-  solution = '.'*11+'ABCD'*2 if part1 else '.'*11+'ABCD'*4
+  solution = '.'*11+'ABCD'*2 if len(puzzle) == 19 else '.'*11+'ABCD'*4
   while queue:
     cost, state = heappop(queue)
-    if state == solution:
-      return cost
-    for i1, i2 in possible_moves(state, part1):
-      new_cost = cost + distances[(i1, i2)] * energy[state[i1]]
-      moved = swap(i1, i2, state)
-      if seen.get(moved, 999999) <= new_cost:
-        continue
+    if state == solution: return cost
+    for a, b in possible_moves(state, hallway, stepout, target):
+      a1,b1 = (a,b) if a < b else (b,a)
+      distance = abs(stepout[targetI[b1]] - a1) + (b1-7)//4
+      new_cost = cost + distance * energy[state[a]]
+      moved = move(a, b, state)
+      if seen.get(moved, 999999) <= new_cost: continue
       seen[moved] = new_cost
       heappush(queue, (new_cost, moved))
-
-energy = dict(A=1, B=10, C=100, D=1000)
-hallway = [0, 1, 3, 5, 7, 9, 10]
-stepout = {"A": 2, "B": 4, "C": 6, "D": 8}
-left = {2: [1, 0], 4: [3, 1, 0], 6: [5, 3, 1, 0], 8: [7, 5, 3, 1, 0]}
-right = {2: [3, 5, 7, 9, 10], 4: [5, 7, 9, 10], 6: [7, 9, 10], 8: [9, 10]}
-targets1 = {"A": range(11, 16, 4), "B": range(
-    12, 17, 4), "C": range(13, 18, 4), "D": range(14, 19, 4)}
-targets2 = {"A": range(11, 24, 4), "B": range(
-    12, 25, 4), "C": range(13, 26, 4), "D": range(14, 27, 4)}
-targetsI = {v: key for key, val in targets2.items() for v in val}
-distances = gen_distances()
 
 
 start = pfc()
 print(solve(read_puzzle("Tag_23_a.txt")))
-print(solve(read_puzzle("Tag_23_b.txt"), False))
-print(pfc() - start)
+print(solve(read_puzzle("Tag_23_b.txt")))
+print(pfc()-start)
